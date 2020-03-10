ae9ffcd5a9e968501665f561f770db1b7648b224
tomicapretto <tomicapretto@gmail.com>
2020-03-10 17:13:40 -0300

    Automatic installation of packages (2)

--- simulation/R/app/server.R
+++ simulation/R/app/server.R
@@ -1,8 +1,17 @@
 # Load libraries ---------------------------------------------------------------
+check_pkgs <- function(pkgs, quietly = FALSE) { 
+  
+  lapply(pkgs, FUN = function(x) {
+    if (!require(x, character.only = TRUE, quietly = quietly)) 
+      install.packages(x, dependencies = TRUE, 
+                       repos = "http://cran.us.r-project.org")
+    library(x, character.only = TRUE, quietly = quietly)
+  })
+  
+}
 
 pkgs <- c("shiny", "shinyBS", "shinyWidgets", "ggplot2", "dplyr")
 check_pkgs(pkgs)
-
 source("utils.R")
 source("server/helpers.R")
 
